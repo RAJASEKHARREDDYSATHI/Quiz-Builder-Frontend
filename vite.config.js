71b3a5cddf3005aab193bf6e7acb7f985efece3e
SATHI.RAJA SEKHAR REDDY <sathirajasekhar9@gmail.com>
2025-09-22 11:56:22 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,5 @@ import react from '@vitejs/plugin-react'
 // then set base to '/frontend/'
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
- base: '/frontend/',   // <-- add this
-=======
-  base: '/frontend/',   // <-- add this
->>>>>>> 3628666548dec35a7874804ff50986c51258a350
+  base: '/frontend/',   // <-- keep this
 })
